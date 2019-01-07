d1fde94033a4eb282b744afa3db6fc212c61681f
ankittyagi54 <tyagiankit54@gmail.com>
2019-01-07 13:21:05 +0530

    meassage

---
+++ src/practicejavaprogramming_package/Variable.java
@@ -0,0 +1,84 @@
+package practicejavaprogramming_package;
+
+public class Variable {
+
+	public static void main(String args[]) 
+	
+	{
+		// TODO Auto-generated method stub
+		
+		/*      int A= 10;                   // name of variables with uppercase letters
+		        int B= 20; 
+		        int C= A+B;
+		     System.out.println(" result is = " +C);     */
+		
+		
+		/*   int a= 10;                   // name of variables with lowercase letters
+		     int b= 20; 
+		     int c= a+b;
+		    System.out.println(" result is = " +c);    */
+		
+
+		
+	/*	int $a= 10;                   // name of variables with dollar letters
+		int $b= 20; 
+		int $c= $a+$b;
+		System.out.println(" result is = " +$c);*/
+		
+		
+	/*	int _$a= 10;                   // name of variables with underscore and dollar letters
+		int _$b= 20; 
+		int _$c= _$a+_$b;
+		System.out.println(" result is = " +_$c); */
+		
+	/*	int __a= 10;                   // name of variables with double underscore
+		int __b= 20; 
+		int __c= __a+__b;
+		System.out.println(" result is = " +__c); */
+		
+	/*	int __a= 10;                   // name of variables with double underscore
+		int __b= 20; 
+		int __c= __a+__b;
+		System.out.println(" result is = " +__c); */
+		
+	/*	int $$a= 10;                   // name of variables with  double dollar letters
+		int $$b= 20; 
+		int $$c= $$a+$$b;
+		System.out.println(" result is = " +$$c); */
+		
+	/*	int $123= 10;                   // name of variables with dollar letters and numeric combination
+		int $234= 20; 
+		int $456= $123+$234;
+		System.out.println(" result is = " +$456); */
+		
+	/*	int $>= 10;                   // name of variables will not accept special charaters
+		int $<= 20; 
+		int $&= $>+$<;
+		System.out.println(" result is = " +$&); */
+		
+	/*	int $$123= 10;                   // name of variables with dollar letters and numeric combination
+		int $$234= 20; 
+		int $$456= $$123+$$234;
+		System.out.println(" result is = " +$$456); */
+		
+		
+
+		/*int __$$123= 10;                 
+		int __$$234= 20; 
+		int __$$456=__$$123+__$$234;
+		System.out.println(" result is = " +__$$456);  */
+		
+		
+		
+		
+		
+	}
+
+		
+		
+		
+		
+		
+	}
+
+}
